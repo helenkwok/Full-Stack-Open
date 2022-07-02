b813e381838b2fafbe9e27b60d39b70a2e9c9d55
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-02 11:52:43 +0930

    2.3*: Course information step8

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,7 +1,7 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
 const Total = ({parts}) => {
-  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises + parts[3].exercises
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
   return <h4>total of {total} exercises</h4>
 }
 
