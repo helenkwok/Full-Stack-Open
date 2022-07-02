22a138d6dc599516fc717da669b4ef9b15c767d0
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-02 18:46:14 +0930

    2.14*: Data for countries, step3

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -8,24 +8,29 @@ const Countries = ({countries, filter, setFilter}) => {
     setFilteredCountries(countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase(), 0)))
   }, [filter])
 
-  return (
-    <div>
-      {
-        filteredCountries.length > 1 && filteredCountries.length <= 10 ?
-        filteredCountries.map(country =>
-          <p key={country.name.common}>
-            {country.name.common}
-            <button onClick={() => setFilter(country.name.common)}>show</button>
-          </p>
-        )
-        :
-        filteredCountries.length === 1?
-          <Country country={filteredCountries[0]} />
-        :
-        <div>Too many matches, specify another filter</div>
-      }
-    </div>
-  )
+  switch(filteredCountries.length) {
+    case 0:
+      return <div>No match, specify another filter</div>
+    case 1:
+      return <Country country={filteredCountries[0]} />
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+      return filteredCountries.map(country =>
+        <p key={country.name.common}>
+          {country.name.common}
+          <button onClick={() => setFilter(country.name.common)}>show</button>
+        </p>
+      )
+    default:
+      return <div>Too many matches, specify another filter</div>
+  }
 }
 
 export default Countries
--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,17 +1,40 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const Country = ({country}) => {
+  const [temp, setTemp] = useState(0)
+  const [weatherIcon, setWeatherIcon] = useState(``)
+  const [weatherDescription, setWeatherDescription] = useState(``)
+  const [wind, setWind] = useState(0)
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${process.env.REACT_APP_API_KEY}`)
+      .then(response => {
+        setTemp(Number(response.data.main.temp-273.15).toFixed(2))
+        setWeatherIcon(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`)
+        setWeatherDescription(response.data.weather[0].description)
+        setWind(response.data.wind.speed)
+      })
+  }, [country])
+
   return (
     <div>
-          <h1>{country.name.common}</h1>
-          <p>{`capital ${country.capital}`}</p>
-          <p>{`area ${country.area}`}</p>
-          <h3>languages:</h3>
-          <ul>
-            {Object.keys(country.languages).map(key =>
-              <li key={key}>{country.languages[key]}</li>
-            )}
-          </ul>
-          <img src={country.flags.png} alt={country.name.common} />
-        </div>
+      <h1>{country.name.common}</h1>
+      <p>{`capital ${country.capital}`}</p>
+      <p>{`area ${country.area}`}</p>
+      <h3>languages:</h3>
+      <ul>
+        {Object.keys(country.languages).map(key =>
+          <li key={key}>{country.languages[key]}</li>
+        )}
+      </ul>
+      <img src={country.flags.png} alt={country.name.common} />
+      <h2>Weather in {country.capital}</h2>
+      <p>{`temperature ${temp} Celsius`}</p>
+      <img src={weatherIcon} alt={weatherDescription} />
+      <p>{`wind ${wind} m/s`}</p>
+    </div>
   )
 }
 
